92f5c3c046fefd85cbf83b29f620eb5e1a8a6215
Safin Ramil <RamilSafnab1996@gmail.com>
2018-03-22 12:57:48 +0300

    Refactor

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,19 @@ int main(int argc, char **argv) {
     initLogger(log4cpp::Priority::DEBUG);
     av_log_set_level(AV_LOG_INFO);
 
-    auto transcoder3 = LIRS::Transcoder::newInstance("/dev/video0", "camera", 744, 480, "bayer_grbg8", "yuv420p", 15, 3, 5);
-
-//    auto transcoder2 = LIRS::Transcoder::newInstance("/dev/video2", "camera2", 744, 480, "bayer_grbg8", "yuv420p", 15, 3, 5);
-//    auto transcoder1 = LIRS::Transcoder::newInstance("/dev/video1", "camera1", 744, 480, "bayer_grbg8", "yuv420p", 15, 3, 5);
-//    auto transcoder0 = LIRS::Transcoder::newInstance("/dev/video0", "camera0", 1280, 720, "yuyv422", "yuv420p", 50, 10, 5);
+    auto transcoder3 = LIRS::Transcoder::newInstance("/dev/video0", "camera0", 744, 480, "bayer_grbg8", "yuv420p", 15, 3, 5);
+    auto transcoder2 = LIRS::Transcoder::newInstance("/dev/video2", "camera2", 744, 480, "bayer_grbg8", "yuv420p", 15, 3, 5);
+    auto transcoder1 = LIRS::Transcoder::newInstance("/dev/video3", "camera3", 744, 480, "bayer_grbg8", "yuv420p", 15, 3, 5);
+    auto transcoder0 = LIRS::Transcoder::newInstance("/dev/video1", "cam_central", 1280, 720, "yuyv422", "yuv420p", 50, 10, 5);
 
     auto server = new LIRS::LiveCameraRTSPServer();
-//    server->addTranscoder(transcoder1);
-//    server->addTranscoder(transcoder2);
+    server->addTranscoder(transcoder1);
+    server->addTranscoder(transcoder2);
     server->addTranscoder(transcoder3);
-//    server->addTranscoder(transcoder0);
+    server->addTranscoder(transcoder0);
 
     server->run();
-//
+
     delete server;
 
     return 0;
